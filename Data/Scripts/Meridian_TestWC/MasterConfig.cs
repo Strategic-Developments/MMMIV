3690ba6a3107e641a45926a7780fecd936899bb7
RELIKT77 <43426243+RELIKT77@users.noreply.github.com>
2026-01-17 18:34:28 -0700

    IniCommit
    hi

---
+++ Data/Scripts/Meridian_TestWC/MasterConfig.cs
@@ -0,0 +1,20 @@
+﻿namespace Scripts
+{
+    partial class Parts
+    {
+        internal Parts()
+        {
+            // naming convention: WeaponDefinition Name
+            //
+            // Enable your definitions using the follow syntax:
+            // PartDefinitions(Your1stDefinition, Your2ndDefinition, Your3rdDefinition);
+            // PartDefinitions includes both weapons and phantoms
+            PartDefinitions(
+                LG_HeavyAutocannonTurret,LG_SuperRailgunTurret,ApophisRailgun
+            );
+            // ArmorDefinitions(Armor1, Armor2);
+            // SupportDefinitions(ArmorEnhancer1A);
+            // UpgradeDefinitions(Upgrade75a, Upgrade75b);
+        }
+    }
+}
