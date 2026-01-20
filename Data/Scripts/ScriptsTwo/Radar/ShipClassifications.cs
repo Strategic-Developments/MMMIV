a0ead40fc4f716cf6c6106ebb4eda9a314ecc5be
Nerd-eee <nerd1234ee45e@gmail.com>
2026-01-19 20:37:57 -0800

    do radar "classes"

--- Data/Scripts/ScriptsTwo/Radar/ShipClassifications.cs
+++ Data/Scripts/ScriptsTwo/Radar/ShipClassifications.cs
@@ -16,7 +16,7 @@ namespace NerdRadar.ExampleMod
             // Feel free to change the Example_ShipClassificationStats to something else, along with the namespace (NerdRadar.ExampleMod)
             // Aside from those, do not change anything above this line
 
-            ReplaceClasses = false, // set to true to overwrite ALL previous classes from any other mod loaded before this one
+            ReplaceClasses = true, // set to true to overwrite ALL previous classes from any other mod loaded before this one
                                     // set to false to add these classes to the existing list
                                     // Weird and unintended behavior may occur when false and adding a class with exactly the same max blockcount as another
 
@@ -31,16 +31,28 @@ namespace NerdRadar.ExampleMod
             */
             LargeGridClasses = new List<ShipClass>() // list of large grid ship classes
             {
-                new ShipClass(3750, "Auxiliary Cruiser"), // first number is the MAXIMUM blockcount, the second is the class name.
-                new ShipClass(15000, "Fleet Battleship"), // first number is the MAXIMUM blockcount, the second is the class name.
+                new ShipClass(250, "Tiny Signature"), // first number is the MAXIMUM blockcount, the second is the class name.
+                new ShipClass(1500, "Small Signature"), // first number is the MAXIMUM blockcount, the second is the class name.
+                new ShipClass(3000, "Medium Signature"), // first number is the MAXIMUM blockcount, the second is the class name.
+                new ShipClass(6000, "Large Signature"), // first number is the MAXIMUM blockcount, the second is the class name.
+                new ShipClass(12000, "Very Large Signature"), // first number is the MAXIMUM blockcount, the second is the class name.
+                new ShipClass(int.MaxValue, "Extreme Signature"), // first number is the MAXIMUM blockcount, the second is the class name.
             },
             SmallGridClasses = new List<ShipClass>() // list of small grid ship classes
             {
-                new ShipClass(25, "Debris"), // first number is the MAXIMUM blockcount, the second is the class name.
+                new ShipClass(500, "Tiny Signature"), // first number is the MAXIMUM blockcount, the second is the class name.
+                new ShipClass(2500, "Small Signature"), // first number is the MAXIMUM blockcount, the second is the class name.
+                new ShipClass(4500, "Medium Signature"), // first number is the MAXIMUM blockcount, the second is the class name.
+                new ShipClass(int.MaxValue, "Large Signature"), // first number is the MAXIMUM blockcount, the second is the class name.
             },
             StationClasses = new List<ShipClass>() // list of station classes, whether large or small grid (yes small grid stations are possible)
             {
-                new ShipClass(20000, "City"), // first number is the MAXIMUM blockcount, the second is the class name.
+                new ShipClass(250, "Tiny Signature"), // first number is the MAXIMUM blockcount, the second is the class name.
+                new ShipClass(1500, "Small Signature"), // first number is the MAXIMUM blockcount, the second is the class name.
+                new ShipClass(3000, "Medium Signature"), // first number is the MAXIMUM blockcount, the second is the class name.
+                new ShipClass(6000, "Large Signature"), // first number is the MAXIMUM blockcount, the second is the class name.
+                new ShipClass(12000, "Very Large Signature"), // first number is the MAXIMUM blockcount, the second is the class name.
+                new ShipClass(int.MaxValue, "Extreme Signature"), // first number is the MAXIMUM blockcount, the second is the class name.
             },
         };
         // Do not modify below this line
