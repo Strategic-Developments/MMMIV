e5347baa3d184c2cd771407a5058f4aefe817af5
Nerd-eee <nerd1234ee45e@gmail.com>
2026-01-25 19:28:38 -0800

    Buff missile power req/nerf PD power req

--- Data/Scripts/ScriptsTwo/CoreParts/03_Missiles/00_Jackal/Weapons.cs
+++ Data/Scripts/ScriptsTwo/CoreParts/03_Missiles/00_Jackal/Weapons.cs
@@ -133,7 +133,7 @@ namespace Scripts
                     HomeElevation = 0, // Default resting elevation
                     FixedInventorySize = true, // If true, the inventory size will be forced to the exact value specified above regardless of world inventory multipliers
                     InventorySize = 1.5f, // Inventory capacity in kL.
-                    IdlePower = 80, // Constant base power draw in MW.
+                    IdlePower = 60, // Constant base power draw in MW.
                     FixedOffset = false, // Deprecated.
                     Offset = Vector(x: 0, y: 0, z: 0), // Offsets the aiming/firing line of the weapon, in metres.
                     Type = BlockWeapon, // What type of weapon this is; BlockWeapon, HandWeapon, Phantom 
