16ca11fcc582fe7a4aa7c491bf6b2e9a79aff41d
RELIKT77 <43426243+RELIKT77@users.noreply.github.com>
2026-02-25 00:43:03 -0700

    kothstruct change + set buff lol

--- Data/Scripts/ScriptsTwo/CoreParts/02_Railguns/00_Set/Weapons.cs
+++ Data/Scripts/ScriptsTwo/CoreParts/02_Railguns/00_Set/Weapons.cs
@@ -161,11 +161,11 @@ namespace Scripts
                 },
                 Loading = new LoadingDef
                 {
-                    RateOfFire = 15, // Set this to 3600 for beam weapons. This is how fast your gun fires per minute.
+                    RateOfFire = 30, // Set this to 3600 for beam weapons. This is how fast your gun fires per minute.
                     BarrelsPerShot = 1, // How many muzzles will fire a projectile per fire event.
                     TrajectilesPerBarrel = 1, // Number of projectiles per muzzle per fire event.
                     SkipBarrels = 0, // Number of muzzles to skip after each fire event.
-                    ReloadTime = 4 * 60, // Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
+                    ReloadTime = 120, // Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
                     MagsToLoad = 1, // Number of physical magazines to consume on reload.
                     DelayUntilFire = 2 * 60, // How long the weapon waits before shooting after being told to fire. Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
                     HeatPerShot = 400000, // Heat generated per shot (BarrelsPerShot * HeatPerShot is the total heat per firing event).
